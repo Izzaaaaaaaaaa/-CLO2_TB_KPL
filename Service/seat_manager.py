ee0c9a720694ea531bd93ec9f0b0efffac593425
pradana argo pangestu <60965994+Pradana123@users.noreply.github.com>
2025-04-24 21:18:58 +0700

    Update seat_manager.py

--- Service/seat_manager.py
+++ Service/seat_manager.py
@@ -1,14 +1,1 @@
-class SeatManager:
-    def __init__(self):
-        self.seats = {
-            'regular': [True]*50,
-            'premium': [True]*30,
-            'vip': [True]*20
-        }
-    
-    def auto_select_seat(self, seat_type):
-        available = self.seats[seat_type].index(True)
-        if available != -1:
-            self.seats[seat_type][available] = False
-            return f"{seat_type.upper()}-{available+1}"
-        return None
+
